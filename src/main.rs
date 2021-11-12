14fd492067b052427047266b3894769af283501b
mystique09 <knightkrtpto7@gmail.com>
2021-11-12 19:33:46 +0800

    format code

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,37 @@
 use std::io;
 
-fn sing(bottles: u32){
-    for i in (0..=bottles).rev() {
-        println!("{}", read_bottles(i));
-    }
+fn sing(bottles: u32) {
+  for i in (0..= bottles).rev() {
+    println!("{}", read_bottles(i));
+  }
 }
 
 fn read_bottles(bottles: u32) -> String {
-    match bottles {
-        2 => {
-            format!("{} bottles of beer on the wall.\n{} bottles of beer. Take one down, pass it around.\n{} bottle of beer on the wall!\n", bottles, bottles, bottles - 1)
-        },
-        1 => {
-            format!("{} bottle of beer on the wall.\n{} bottle of beer. Take one down, pass it around.\n{} bottles of beer on the wall!\n", bottles, bottles, bottles - 1)
-        },
+  match bottles {
+    2 => {
+      format!("{} bottles of beer on the wall.\n{} bottles of beer. Take one down, pass it around.\n{} bottle of beer on the wall!\n", bottles, bottles, bottles- 1)
+    },
+    1 => {
+      format!("{} bottle of beer on the wall.\n{} bottle of beer. Take one down, pass it around.\n{} bottles of beer on the wall!\n", bottles, bottles, bottles- 1)
+    },
     0 => String::from("Let's do it again!\n"),
     _u32 => {
-        format!("{} bottles of beer on the wall.\n{} bottles of beer. Take one down, pass it around.\n{} bottles of beer on the wall.\n", bottles, bottles, bottles - 1)
+      format!("{} bottles of beer on the wall.\n{} bottles of beer. Take one down, pass it around.\n{} bottles of beer on the wall.\n", _u32, _u32, _u32 - 1)
     }
-    }
+  }
 }
 
 fn main() {
-   let mut input = String::new();
-   
-   println!("Enter the number of bottles: ");
+  let mut input = String::new();
 
-   io::stdin().read_line(&mut input).expect("Unable to read line");
+  println!("Enter the number of bottles: ");
 
-   match input.trim().parse() {
-       Ok(bottles) => sing(bottles),
-       Err(_why) => {
-           println!("Invalid input, enter a number");
-       }
-   }
+  io::stdin().read_line(&mut input).expect("Unable to read line");
+
+  match input.trim().parse() {
+    Ok(bottles) => sing(bottles),
+    Err(_why) => {
+      println!("Invalid input, enter a number");
+    }
+  }
 }
